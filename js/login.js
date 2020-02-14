7c89e3311df074d31414d80d2ac83b415d9dcec4
Nikita Kirad <2nov.nk@gmail.com>
2020-02-14 18:44:57 +0530

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,23 @@
+function getLoginInfo() {
+    let userName=document.getElementById("username").value;
+    let passWord=document.getElementById("password").value;
+    console.log(userName);
+    let data = JSON.parse(localStorage.getItem(userName));
+    console.log(data);
+    if(data.uname==userName && data.password==passWord)
+    {
+        console.log("login successful");
+        alert("Login successful");
+       sessionStorage.setItem("user",userName);
+        let data1=sessionStorage.getItem("user")
+        console.log(data1);
+
+    }
+    else{
+        alert("Invalid entries");
+
+       
+    }
+        
+    
+}
---
+++ js/profile.js
@@ -0,0 +1,10 @@
+function display(){
+    // let data1=sessionStorage.getItem("user");
+    // console.log(data1);
+    let data = JSON.parse(localStorage.getItem(sessionStorage.getItem("user")));
+    // if(data.uname==data1){
+    //     console.log("hii")
+    // }
+    console.log("hii");
+    console.log("hii");console.log("hii");
+}
